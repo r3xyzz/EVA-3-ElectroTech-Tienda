b4dfa3fe4149d37b57c38bea3678583df30306ad
Usuario <Usuario@DESKTOP-OBO6H78>
2023-11-20 16:39:19 -0300

    Precio en "agregarProducto()", "String SQL", cambiado "Precio" a "Precios" ARACELI

--- src/Negocio/Product.java
+++ src/Negocio/Product.java
@@ -119,7 +119,7 @@ public class Product {
             SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
             String fechaFormateada = formato.format(fecha);
         
-            String sql = "INSERT INTO PRODUCT (Nombre, Marca, Categoria, Precio, Cantidad_Stock, Fecha_Adquisicion) VALUES ('"+nombre+"', '"+marca+"', '"+categoria+"',"+precio+", "+cantidadEnStock+",'"+fechaFormateada+"')";
+            String sql = "INSERT INTO PRODUCT (Nombre, Marca, Categoria, Precios, Cantidad_Stock, Fecha_Adquisicion) VALUES ('"+nombre+"', '"+marca+"', '"+categoria+"',"+precio+", "+cantidadEnStock+",'"+fechaFormateada+"')";
 
             ElectroTech.conectar();
             ElectroTech.sentencia = ElectroTech.conexion.prepareStatement(sql);
@@ -128,7 +128,7 @@ public class Product {
             ElectroTech.desconectar();
         }catch (Exception e){
             System.out.println(e);
-            System.out.println("Error al agregar pelicula");
+            System.out.println("Error al agregar PRODUCTO");
         }
     }
 
